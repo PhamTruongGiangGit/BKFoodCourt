30c7ea72d716acecadecd128d323eff179417830
tphiepbk <itmanagertph@gmail.com>
2020-06-24 15:30:25 +0700

    After demo

--- app/src/main/java/com/example/bkfoodcourt/MainActivity.kt
+++ app/src/main/java/com/example/bkfoodcourt/MainActivity.kt
@@ -15,9 +15,9 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.firebase.auth.AuthResult
-import com.google.firebase.auth.FirebaseAuth
+//import com.google.android.gms.tasks.OnCompleteListener
+//import com.google.firebase.auth.AuthResult
+//import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_main.*
 import com.example.bkfoodcourt.Common.Common
@@ -36,6 +36,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // Configure Google Sign In
+        /**
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(getString(R.string.default_web_client_id))
             .requestEmail()
@@ -46,6 +47,7 @@ class MainActivity : AppCompatActivity() {
             var signInIntent = googleSignInClient?.signInIntent
             startActivityForResult(signInIntent, RC_SIGN_IN)
         }
+        ***/
 
         /** Call function to check network connection on start up **/
         if (!checkNetwork(this)) {
